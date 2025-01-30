b6976e8cbbdec1d32c6915d56697eaa1c517d390
Exanlv <exan@landviz.nl>
2025-01-30 12:44:46 +0100

    Use internal reject promise exception

--- src/Connector.php
+++ src/Connector.php
@@ -8,9 +8,9 @@ use React\EventLoop\LoopInterface;
 use React\Socket\ConnectionInterface;
 use React\Socket\ConnectorInterface;
 use React\Promise\Deferred;
-use React\Promise\RejectedPromise;
 use Psr\Http\Message\RequestInterface;
 use GuzzleHttp\Psr7 as gPsr;
+use React\Promise\Internal\RejectedPromise;
 
 #[AllowDynamicProperties]
 class Connector {
